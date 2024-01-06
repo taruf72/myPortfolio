f4c6b02e115836d45c6fc36a90c8660251b86e13
taruf72 <taruf.imgglobalinfotech@gmail.com>
2024-01-06 21:56:17 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["a Programmer.", "a Web Developer.","an Artist.","a Btech Student.", "Android Developer."],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+});
